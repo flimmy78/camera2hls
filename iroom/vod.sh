db2c0b1dc22db4763893a0a2153d59c91e889724
zhiguangq <root@iZuf6gql693jaz1y19agh1Z.(none)>
2018-01-24 15:38:43 +0800

    add vod enc

--- iroom/vod.sh
+++ iroom/vod.sh
@@ -5,6 +5,6 @@ echo $2
 
 cd /root/zhiguangq/camera2hls/iroom/objs/nginx/html/live
 if [ ! -f "$2.m3u8" ]; then
-	ffmpeg  -i $1 -vcodec copy -an -f hls -hls_playlist_type vod -hls_time 10 $2.m3u8 2>/dev/null &
+	ffmpeg  -i $1 -vcodec libx264 -vprofile baseline -g 25 -s 704x576 -an -f hls -hls_list_size 0 -hls_time 2 -y -threads 4 $2.m3u8 2>/dev/null &
 fi
 
